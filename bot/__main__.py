4934e2f3a6677c3b1cee64c025b091a642c23201
Absiddiq3 <107870204+Absiddiq3@users.noreply.github.com>
2022-08-24 17:20:00 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-HELLO MASTER ABUBAKAR ⚔️
-I AM AT YOUR SERVICE
+<b>HELLO MASTER ABUBAKAR</b> ⚔️
+<b>I AM AT YOUR SERVICE</b>
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     #else:
